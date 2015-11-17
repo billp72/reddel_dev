da9fc863abb59dfb1e91ec169aacc6d47e5d0f8b
netcreative <epop.bill@gmail.com>
2015-11-16 21:22:11 -0500

    filter by conversationStarted

--- www/js/filters.js
+++ www/js/filters.js
@@ -1,8 +1,23 @@
-angular.module('mychat.filters', [])
+var filt = angular.module('mychat.filters', [])
 
-.filter('reverse', [function () {
+filt.filter('reverse', [function () {
   return function(items) {
   	if( !items ){ return; }
          return items.slice().reverse();
   };
+}]);
+
+filt.filter('conversation', [function () {
+	return function(items, attr){
+		if(!items){ return;}
+		 var shownItems = [];
+		 angular.forEach(items, function (item) {
+    
+                if (item[attr] === true) {shownItems.unshift(item)}
+            
+                if (item[attr] !== true) {shownItems.push(item)}
+           
+        });
+		return shownItems;
+	}
 }]);
